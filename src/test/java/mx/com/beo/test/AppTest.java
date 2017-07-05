7cec88c82e7099e13d6802e719e47c9c4271b304
novasolutionsystems <adelgado@novasolutionsystems.com>
2017-07-04 20:58:56 -0500

    prueba unitaria

--- src/test/java/mx/com/beo/test/AppTest.java
+++ src/test/java/mx/com/beo/test/AppTest.java
@@ -64,17 +64,8 @@ public class AppTest
         			+ "\"confirmNewPassword\": \"1\""
         			+ "}";
         	 
-        	String urlContrasena=System.getenv("protocolo2")+"://"+System.getenv("basepath2")+":"+System.getenv("puerto2")+"/"+System.getenv("servicio2");
+        	String urlContrasena="http://beo-mockup/cambioContrasena";
   
-//        	String urlContrasena="http://200.39.24.141/BEO/cambioContrasena";
-     
-     
-     
-// http://200.39.24.141/BEO/cambioContrasena
-        	
-//        	---------------------------------------------------------------------------------------
-        	
-        	
         	
         	 given(). 
         			headers(mapContrasena)
@@ -85,130 +76,7 @@ public class AppTest
     				.log().all(); 
     		
     		
-    		
-    		
-//    		-------------------------------------Consulta datos basicos-.------------------------------------------
-
-
-            System.out.println("incio Consulta datos basicos");
-
-    		Map<String,Object> map=new HashMap<String, Object>();
-        	 
-        	
-        	String body="{"
-        			+ "\"idPersona\": \"1025478963\","
-        					+ "\"ticket\": {"
-        							+ "\"id_user\": \"\","
-        									+ "\"id_creds\": \"\""
-        											+ "},"
-        											+ "\"canal\": \"CUBA1234575SDF\""
-        													+ "}";
-        	 
-        	String url=System.getenv("protocolo5")+"://"+System.getenv("basepath5")+":"+System.getenv("puerto5")+"/"+System.getenv("servicio5");
-        	
-//        	String url= "http://200.39.24.141/BEO/consultaDatosBasicos";
-        	
-        	
-//"http://200.39.24.141/BEO/consultaDatosBasicos";
-        	
-        	
-        	
-        	
-        	
-//        	---------------------------------------------------------------------------------------
-        	
-        	System.out.println("Inicio consulta Servicios Contratados");
-        	
-        	 given(). 
-        			headers(map)
-    				.contentType("application/json").body(body)
-    			
-    				.when().post(url)
-    				.then()
-    				.log().all(); 
-        	 
-        	 
-        	 System.out.println("Inicio consultaServiciosContratados ");
-//        	 -------------------------------------------------------------------
-//        	 ---------------------consultaServiciosContratados----------------------------------------------
-        	 Map<String,Object> mapAccesoCLi=new HashMap<String, Object>();
-        	 
-        	  
-     
-        	String urlAccesoCli=System.getenv("protocolo6")+"://"+System.getenv("basepath6")+":"+System.getenv("puerto6")+"/"+System.getenv("servicio6");
-        	 
  
-//        	String urlAccesoCli="http://200.39.24.141/BEO/consultaServiciosContratados";
-         	 
-         	 given(). 
-         			headers(mapAccesoCLi)
-     				.contentType("application/json").body(body)
-     			
-     				.when().post(urlAccesoCli)
-     				.then()
-     				.log().all(); 
-
-
-  System.out.println("envio notificación"); 
-//	 -------------------------------------------------------------------
-//	 ---------------------envio notificacion----------------------------------------------
-	 Map<String,Object> mapNotificacion=new HashMap<String, Object>();
-	
-
-
-	 String urlNotificacion=System.getenv("protocolo3")+"://"+System.getenv("basepath3")+":"+System.getenv("puerto3")+"/"+System.getenv("servicio3");
- 
-//	String urlNotificacion="http://200.39.24.141/BEO/envioNotificaciones"; 
-//	---------------------------------------------------------------------------------------
-	
-	String bodyNotificacion="{"
-			+ "\"idPersona\": \"2222\","
-			+ "\"ticket\": {"
-			+ "\"id_user\": \"123\","
-			+ "\"id_creds\": \"123\""
-			+ "},"
-			+ "\"tipoMensaje\": \"mensaje\","
-			+ "\"tipoNotificacion\": \"notificacion\","
-			+ "\"from\": \"123\","
-			+ "\"to\": \"321\","
-			+ "\"mapParameters\": {"
-			+ "\"key\": \"1223333\""
-			+ " },"
-			+ "\"canal\": \"1\""
-			+ "}";
-	
-	
-	 given(). 
-			headers(mapNotificacion)
-			.contentType("application/json").body(bodyNotificacion)
-		
-			.when().post(urlNotificacion)
-			.then()
-			.log().all(); 
-	 
-//	 ----------------------------------------------------------------------------------------------------------------
-	 System.out.println("Inicio Valida contrato ");
-//	 -------------------------------------------------------------------
-//	 ---------------------consultaServiciosContratados----------------------------------------------
-	 Map<String,Object> mapValidacontrato=new HashMap<String, Object>();
-	 
-	 String urlValidaContrato=System.getenv("protocolo4")+"://"+System.getenv("basepath4")+":"+System.getenv("puerto4")+"/"+System.getenv("servicio4"); 
-	 
-//	String urlValidaContrato="http://200.39.24.141/BEO/Validacontrato";
- 
-  
-		String bodyValidaContrato="{"
-								+ " \"Usuario\": 1,"
-								+ "	 \"ContratoAceptado\": 1"
-								+ "}";
- 	 
- 	 given(). 
- 			headers(mapValidacontrato)
-				.contentType("application/json").body(bodyValidaContrato)
-			
-				.when().post(urlValidaContrato)
-				.then()
-				.log().all();
 
     		
     	} catch (Exception e) {
